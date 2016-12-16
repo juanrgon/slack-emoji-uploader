79ec31738d626d1905e5a3e2c13459da3d9e1b7d
juanrgon <jrg2156@gmail.com>
2016-12-16 02:20:25 -0500

    1.1.9: Only save slack team if the team is valid

--- options.js
+++ options.js
@@ -1,31 +1,6 @@
 // Saves options to chrome.storage.sync.
 function save_options() {
   var team_domain = document.getElementById('team-domain').value;
-  validate_team(team_domain);
-  chrome.storage.sync.set({
-    team_domain: team_domain,
-  }, function () {
-    // Update status to let user know options were saved.
-    var status = document.getElementById('settings_status');
-    status.textContent = 'Options saved!';
-    status.style.color = 'green';
-    setTimeout(function () {
-      status.textContent = '';
-    }, 750);
-  });
-  return true;
-}
-// Restores select box and checkbox state using the preferences
-// stored in chrome.storage.
-function restore_options() {
-  chrome.storage.sync.get({
-    team_domain: 'your-team-name',
-  }, function (items) {
-    document.getElementById('team-domain').value = items.team_domain;
-  });
-}
-// Determine if the team domain is reachable.
-function validate_team(team_domain) {
   var status = document.getElementById('team_status');
   status.textContent = ''
   var xhr = new XMLHttpRequest();
@@ -42,9 +17,18 @@ function validate_team(team_domain) {
           });
           status.textContent = 'Login to team in another tab';
           status.style.color = 'red';
-          return false;
         } else {
-          return true;
+          chrome.storage.sync.set({
+            team_domain: team_domain,
+          }, function () {
+            // Update status to let user know options were saved.
+            var status = document.getElementById('settings_status');
+            status.textContent = 'Options saved!';
+            status.style.color = 'green';
+            setTimeout(function () {
+              status.textContent = '';
+            }, 750);
+          });
         }
       } else {
         status.textContent = 'Unreachable: ' + xhr.status;
@@ -54,6 +38,15 @@ function validate_team(team_domain) {
     }
   }
   xhr.send();
+}
+// Restores select box and checkbox state using the preferences
+// stored in chrome.storage.
+function restore_options() {
+  chrome.storage.sync.get({
+    team_domain: 'your-team-name',
+  }, function (items) {
+    document.getElementById('team-domain').value = items.team_domain;
+  });
 }
 document.addEventListener('DOMContentLoaded', restore_options);
 document.getElementById('save').addEventListener('click', save_options);
