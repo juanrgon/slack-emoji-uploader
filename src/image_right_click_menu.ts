b6c66caa4ca37e021b87a441cdadb499b2c49eb3
Juan Gonzalez <jrg2156@gmail.com>
2022-08-05 15:56:59 +0000

    wip

--- src/image_right_click_menu.ts
+++ src/image_right_click_menu.ts
@@ -1,19 +1,18 @@
 import * as chrome_extension from "./chrome_extension";
 import * as slack_team from "./slack_team";
 
-export const createAllButtons = async () =>  {
-    // First remove all right click menu buttons created by this extension
-	chrome_extension.removeAllRightClickMenuButtons();
+export const createAllButtons = async () => {
+  // First remove all right click menu buttons created by this extension
+  chrome_extension.removeAllRightClickMenuButtons();
 
-    const slack_teams = await slack_team.SlackTeam.loadFromBrowserStorage();
+  const slack_teams = await slack_team.SlackTeam.loadFromBrowserStorage();
 
-    slack_teams.forEach(team => {
-        chrome_extension.addRightClickMenuButton(
-            `add-emoji-to-${team.name}`,
-            `Add emoji to ${team.name}`,
-            ['image'],
-            team.addEmoji
-        )
+  slack_teams.forEach((team) => {
+    chrome_extension.addRightClickMenuButton({
+      id: `add-emoji-to-${team.name}`,
+      title: `Add emoji to ${team.name}`,
+      contexts: ["image"],
+      onClick: team.addEmoji,
     });
-}
-
+  });
+};
