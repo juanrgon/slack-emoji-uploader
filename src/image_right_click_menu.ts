09cffba2bebb11b910181cd6d840ad66054de625
Juan Gonzalez <jrg2156@gmail.com>
2022-08-05 15:35:16 +0000

    fix typo

--- src/image_right_click_menu.ts
+++ src/image_right_click_menu.ts
@@ -2,7 +2,7 @@ import * as chrome_extension from "./chrome_extension";
 import * as slack_team from "./slack_team";
 
 export const createAllButtons = async () =>  {
-    // First remove all right click menu created by this extension
+    // First remove all right click menu buttons created by this extension
 	chrome_extension.removeAllRightClickMenuButtons();
 
     const slack_teams = await slack_team.SlackTeam.loadFromBrowserStorage();
