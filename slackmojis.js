4beb9dace75a180d347ada6d8084695e9879afd3
Juan Gonzalez <jrg2156@gmail.com>
2022-08-05 16:33:38 +0000

    convert tabs to 4 spaces

--- slackmojis.js
+++ slackmojis.js
@@ -1,38 +1,38 @@
 function searchUpForTag(el, tagName) {
-	while (el.tagName !== tagName) {
-		(el = el.parentElement);
-	}
-	return el;
+    while (el.tagName !== tagName) {
+        (el = el.parentElement);
+    }
+    return el;
 }
 
 function getEmojiDetails(clicked_el) {
-	var anchor_tag = searchUpForTag(clicked_el, 'A');
-	var imageEl = anchor_tag.getElementsByTagName('img')[0];
-	var emojiName = anchor_tag.getElementsByClassName('name')[0].innerHTML.split(':')[1];
-	emojiName.strp
-	return {
-		name: emojiName,
-		url: imageEl.src
-	};
+    var anchor_tag = searchUpForTag(clicked_el, 'A');
+    var imageEl = anchor_tag.getElementsByTagName('img')[0];
+    var emojiName = anchor_tag.getElementsByClassName('name')[0].innerHTML.split(':')[1];
+    emojiName.strp
+    return {
+        name: emojiName,
+        url: imageEl.src
+    };
 }
 
 function addEmoji(event) {
-	var clicked_el = event.target;
-	name_and_url = getEmojiDetails(clicked_el);
-	var url = name_and_url.url;
-	var name = name_and_url.name;
-	chrome.runtime.sendMessage({
-		emojiName: name,
-		emojiUrl: url
-	});
+    var clicked_el = event.target;
+    name_and_url = getEmojiDetails(clicked_el);
+    var url = name_and_url.url;
+    var name = name_and_url.name;
+    chrome.runtime.sendMessage({
+        emojiName: name,
+        emojiUrl: url
+    });
 }
 var emojiButtons = document.getElementsByClassName('downloader');
 for (var i = 0; i < emojiButtons.length; i++) {
-	var emojiButton = emojiButtons[i];
-	emojiButton.removeAttribute("download");
-	emojiButton.setAttribute("href", "#");
-	emojiButton.setAttribute("onclick", "return false;")
-	emojiButton.addEventListener("click", function (event) {
-		addEmoji(event);
-	}, false);
+    var emojiButton = emojiButtons[i];
+    emojiButton.removeAttribute("download");
+    emojiButton.setAttribute("href", "#");
+    emojiButton.setAttribute("onclick", "return false;")
+    emojiButton.addEventListener("click", function (event) {
+        addEmoji(event);
+    }, false);
 }
--- src/on_update.ts
+++ src/on_update.ts
@@ -15,13 +15,13 @@ export const onUpdate = (
  * Structure of slack team name storage changed in 1.3.0 to support multiple teams.
  */
 const V_1_3_0__support_multiple_teams = () => {
-	chrome.storage.sync.get({ 'team_domain': null }, function (items) {
-		if (items.team_domain !== null) {
-			chrome.storage.sync.set({ 'teamDomains': [items.team_domain] });
-		}
-		chrome.storage.sync.remove('team_domain');
-	});
+    chrome.storage.sync.get({ 'team_domain': null }, function (items) {
+        if (items.team_domain !== null) {
+            chrome.storage.sync.set({ 'teamDomains': [items.team_domain] });
+        }
+        chrome.storage.sync.remove('team_domain');
+    });
 
-	alert('Slack Emoji Uploader now supports multiple teams!');
-	chrome.runtime.openOptionsPage();
+    alert('Slack Emoji Uploader now supports multiple teams!');
+    chrome.runtime.openOptionsPage();
 }
